f58a3113567d12025c7928acb571ed7d1ca2a2a5
Sean Murphy <sean@iamseanmurphy.com>
2026-08-24 18:56:34 +0000

    2.6.6

--- search-exclude/build/store/js/index.asset.php
+++ search-exclude/build/store/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-data', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => 'e00e34dddeeae3d0bac8');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-data', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => '6a2871dbc9d267d1c763');
